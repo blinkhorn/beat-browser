9957c92abbdfc92d39cb1d2a2c006cee329518af
Patrick Blinkhorn <patrickblinkhorn@gmail.com>
2018-02-10 20:08:18 -0500

    added to model classes + added java dependencies

--- src/main/java/org/springframework/beatbrowser/model/MusicEntity.java
+++ src/main/java/org/springframework/beatbrowser/model/MusicEntity.java
@@ -1,4 +1,20 @@
 package org.springframework.beatbrowser.model;
 
-public class MusicEntity {
+import org.hibernate.validator.constraints.NotEmpty;
+
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
+public class MusicEntity extends BaseEntity {
+
+    @Column(name = "genre")
+    @NotEmpty
+    private String genre;
+
+    @Column(name = "title")
+    @NotEmpty
+    private String title;
+
+    
 }
