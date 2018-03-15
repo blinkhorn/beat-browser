8373815dc884557ca28507a1b137b026d6eb8cc8
Patrick Blinkhorn <patrickblinkhorn@gmail.com>
2018-03-15 14:10:11 -0400

    worked on same artist-release many to many relationship from release.java

--- src/main/java/org/springframework/beatbrowser/release/Release.java
+++ src/main/java/org/springframework/beatbrowser/release/Release.java
@@ -5,15 +5,41 @@ import org.springframework.beans.support.PropertyComparator;
 import org.springframework.beatbrowser.artist.Artist;
 import org.springframework.beatbrowser.model.MusicEntity;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Entity;
-import javax.persistence.ManyToMany;
-import javax.persistence.Table;
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
 import java.util.*;
 
 @Entity
 @Table(name = "releases")
 public class Release extends MusicEntity {
 
+    @Column(name = "numberTracks")
+    @NotEmpty
+    private int numberTracks;
 
+    @ManyToMany(cascade = CascadeType.ALL, mappedBy = "releases")
+    private Set<Artist> artists;
+
+    protected Set<Artist> getArtistsInternal() {
+        if (this.artists == null) {
+            this.artists = new HashSet<Artist>();
+        }
+        return this.artists;
+    }
+
+    protected void getArtistsInternal(Set<Artist> artists) { this.artists = artists; }
+
+    public List<Artist> getArtists() {
+        List<Artist> sortedArtists = new ArrayList<Artist>(getArtistsInternal());
+        PropertyComparator.sort(sortedArtists,
+                new MutableSortDefinition("name", true, true));
+        return Collections.unmodifiableList(sortedArtists);
+    }
+
+    public void addArtist(Artist artist) {
+        if (artist.isNew()) {
+            getArtistsInternal().add(artist);
+        }
+//        artist.setRelease(this);
+    }
 }
