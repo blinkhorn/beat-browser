a1090c44d1e36667298ab7f5364b4ac98676b911
Patrick Blinkhorn <patrickblinkhorn@gmail.com>
2018-03-15 13:53:43 -0400

    chagned artist-release relationship to many to many

--- src/main/java/org/springframework/beatbrowser/artist/Artist.java
+++ src/main/java/org/springframework/beatbrowser/artist/Artist.java
@@ -1,11 +1,13 @@
 package org.springframework.beatbrowser.artist;
 
+import org.springframework.beans.support.MutableSortDefinition;
+import org.springframework.beans.support.PropertyComparator;
 import org.springframework.beatbrowser.model.NamedEntity;
+import org.springframework.beatbrowser.release.Release;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 import javax.validation.constraints.NotEmpty;
+import java.util.*;
 
 @Entity
 @Table(name = "artists")
@@ -18,6 +20,31 @@ public class Artist extends NamedEntity {
     @NotEmpty
     private String nationality;
 
+    @ManyToMany(cascade = CascadeType.ALL, mappedBy = "artist")
+    private Set<Release> releases;
+
+    protected Set<Release> getReleasesInternal() {
+        if (this.releases == null) {
+            this.releases = new HashSet<Release>();
+        }
+        return this.releases;
+    }
+
+    protected void setReleasesInternal(Set<Release> releases) { this.releases = releases; }
+
+    public List<Release> getReleases() {
+        List<Release> sortedReleases = new ArrayList<Release>(getReleasesInternal());
+        PropertyComparator.sort(sortedReleases,
+                new MutableSortDefinition("title", true, true));
+        return Collections.unmodifiableList(sortedReleases);
+    }
+
+    public void addRelease(Release release) {
+        if (release.isNew()) {
+            getReleasesInternal().add(release);
+        }
+//        release.setArtist(this);
+    }
 
 
 }
