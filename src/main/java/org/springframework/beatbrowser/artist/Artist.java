01e546aa19fc36cd3eb6f67610a866a82a22354d
Patrick Blinkhorn <patrickblinkhorn@gmail.com>
2018-03-14 21:23:22 -0400

    added attributes to Artist; about to change to ManyToMany

--- src/main/java/org/springframework/beatbrowser/artist/Artist.java
+++ src/main/java/org/springframework/beatbrowser/artist/Artist.java
@@ -2,11 +2,22 @@ package org.springframework.beatbrowser.artist;
 
 import org.springframework.beatbrowser.model.NamedEntity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
+import javax.validation.constraints.NotEmpty;
 
 @Entity
 @Table(name = "artists")
 public class Artist extends NamedEntity {
-//    @Column
+    @Column(name = "dob") //date of birth
+    @NotEmpty
+    private String dob;
+
+    @Column(name = "nationality")
+    @NotEmpty
+    private String nationality;
+
+
+
 }
--- src/main/java/org/springframework/beatbrowser/release/Track.java
+++ src/main/java/org/springframework/beatbrowser/release/Track.java
@@ -1,4 +1,10 @@
 package org.springframework.beatbrowser.release;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tracks")
 public class Track extends Release {
+
 }
