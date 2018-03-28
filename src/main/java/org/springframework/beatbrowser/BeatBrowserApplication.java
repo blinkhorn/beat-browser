60c2e038120b2d481503157a9f6c50a55a4fb1b3
Patrick Blinkhorn <patrickblinkhorn@gmail.com>
2018-03-27 20:50:13 -0400

    added Spring Boot Application for program

---
+++ src/main/java/org/springframework/beatbrowser/BeatBrowserApplication.java
@@ -0,0 +1,11 @@
+package org.springframework.beatbrowser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeatBrowserApplication {
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(BeatBrowserApplication.class, args);
+    }
+}
--- src/main/java/org/springframework/beatbrowser/label/LabelController.java
+++ src/main/java/org/springframework/beatbrowser/label/LabelController.java
@@ -1,4 +1,8 @@
 package org.springframework.beatbrowser.label;
 
+import org.springframework.stereotype.Controller;
+
+@Controller
 class LabelController {
+
 }
